47870c4af39077b2871d98f1c71c802a61425c7e
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-20 15:47:43 +0530

    User Mdel b4 migrations

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class Blogger(AbstractUser):
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'), unique=True)

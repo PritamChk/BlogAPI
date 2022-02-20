4af882004ccfefe90ebb161586f3918674ed986c
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-21 00:17:59 +0530

    filters working

--- blogapp/filters.py
+++ blogapp/filters.py
@@ -1,39 +1,34 @@
-#FIXME: Whole Filtering 
-# from django_filters import FilterSet
-# # from django_filters import Sea
-# from .models import(
-#     Blogger,
-#     Blog,
-#     Comment,
-# )
+from django_filters import FilterSet
+from .models import(
+    Blogger,
+    Blog,
+    Comment,
+)
 
 
-# class BloggerSearchFilter(FilterSet):
+class BloggerSearchFilter(FilterSet):
+    class Meta:
+        model = Blogger
+        fields = {
+            'first_name': ['istartswith'],
+            'last_name': ['istartswith', 'icontains'],
+            'email': ['icontains'],
+            "username": ["startswith", "exact"],
+            'last_login': ['year__gt', 'year__lt', "month__gt", "month__lt"],
+        }
+
+
+class BlogFilter(FilterSet):
+    class Meta:
+        model = Blog
+        fields = {
+            "title": ["icontains"],
+            "description": ['icontains']
+        }
+
+
+# class CommentFilter(FilterSet):
 #     class Meta:
-#         model = Blogger
-#         fields = {
-#             'first_name': ['istartswith', 'iexact' ],
-#             'last_name': ['istartswith', 'iexact', 'icontains', ],
-#             'email': ['istartswith', 'icontains', 'endswith'],
-#             "username": ["startswith", "exact"],
-#             'last_login': ['exact', 'gt', 'lt'],
-#             # "blogs":[]
-#         }
-
-
-# class BlogFilter(FilterSet):
-#     class Meta:
-#         model = Blog
+#         model = Comment
 #         # TODO : filter fields to be added
-#         fields = {
-#             "title":["icontains"],
-#             "creator":[""]    
-#         }
-    
-
-
-# # class CommentFilter(FilterSet):
-# #     class Meta:
-# #         model = Comment
-# #         # TODO : filter fields to be added
-# #         # fields
+#         # fields
--- blogapp/views.py
+++ blogapp/views.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.views import FilterView
 from .models import Blogger, Blog, Comment
 from .serializer import *
-# from .filters import BloggerSearchFilter, BlogFilter
+from .filters import BloggerSearchFilter, BlogFilter
 
 
 class BloggerViewSet(ModelViewSet, FilterView):
@@ -12,15 +12,11 @@ class BloggerViewSet(ModelViewSet, FilterView):
         "get", "post",
         "patch",
         "delete", "head", "option"]
-    filter_backend = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    # filter_class = BlogFilter
-    filterset_fields = ["first_name"]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_class = BloggerSearchFilter
     search_fields = ["first_name"]
     ordering_fields = ["first_name", "last_name"]
     queryset = Blogger.objects.all()
-
-    # def get_queryset(self):
-    # return Blogger.objects.filter(id=self.kwargs.get('pk'))
 
     def get_serializer_class(self):
         method = self.request.method
@@ -32,3 +28,6 @@ class BloggerViewSet(ModelViewSet, FilterView):
 
     def get_serializer_context(self):
         return {"request": self.request}
+
+#TODO : Implement BlogViewSet 
+#TODO : Implement CommentViewSet

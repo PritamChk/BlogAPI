5e57502f310f93e23626575f1538bf4fa6cd9586
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-21 00:06:06 +0530

    filter thik krete hbe

--- blogapp/filters.py
+++ blogapp/filters.py
@@ -1,32 +1,39 @@
-from django_filters import FilterSet
-from .models import(
-    Blogger,
-    Blog,
-    Comment,
-)
+#FIXME: Whole Filtering 
+# from django_filters import FilterSet
+# # from django_filters import Sea
+# from .models import(
+#     Blogger,
+#     Blog,
+#     Comment,
+# )
 
 
-class BloggerFilter(FilterSet):
-    class Meta:
-        model = Blogger
-        fields = {
-            'first_name': ['istartswith', 'iexact', 'icontains', ],
-            'last_name': ['istartswith', 'iexact', 'icontains', ],
-            'email': ['istartswith', 'icontains', 'endswith'],
-            "username": ["startswith", "exact"],
-            'last_login': ['exact', 'year__gt', 'year__lt'],
-        }
+# class BloggerSearchFilter(FilterSet):
+#     class Meta:
+#         model = Blogger
+#         fields = {
+#             'first_name': ['istartswith', 'iexact' ],
+#             'last_name': ['istartswith', 'iexact', 'icontains', ],
+#             'email': ['istartswith', 'icontains', 'endswith'],
+#             "username": ["startswith", "exact"],
+#             'last_login': ['exact', 'gt', 'lt'],
+#             # "blogs":[]
+#         }
 
 
-class BlogFilter(FilterSet):
-    class Meta:
-        model = Blog
-        # TODO : filter fields to be added
-        # fields
+# class BlogFilter(FilterSet):
+#     class Meta:
+#         model = Blog
+#         # TODO : filter fields to be added
+#         fields = {
+#             "title":["icontains"],
+#             "creator":[""]    
+#         }
+    
 
 
-class CommentFilter(FilterSet):
-    class Meta:
-        model = Comment
-        # TODO : filter fields to be added
-        # fields
+# # class CommentFilter(FilterSet):
+# #     class Meta:
+# #         model = Comment
+# #         # TODO : filter fields to be added
+# #         # fields

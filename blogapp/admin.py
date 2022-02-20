e20b7e9e2886fb38cbf7941f290788ef4b14dcc6
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-20 16:07:39 +0530

    blogger admin done

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Blogger)
+class BloggerAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2','first_name','last_name',"email"),
+        }),
+    )
+    
+    list_filter = (
+        'last_login',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+        'date_joined',
+        "last_login",
+    )
+    
+    prepopulated_fields  = {"username":("last_name","first_name")}
+    raw_id_fields = ('groups', 'user_permissions')

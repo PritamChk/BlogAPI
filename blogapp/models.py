66201c3d3120fd5a41e6984f0094e811d8674bcd
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-20 17:42:03 +0530

    blog model created

--- blogapp/models.py
+++ blogapp/models.py
@@ -10,7 +10,14 @@ class Blogger(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     # follows = models.ManyToManyField('self',blank=True,symmetrical=False)
     # followed_by = models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True,related_name='followers')
+
     class Meta:
         verbose_name = _('blogger')
         verbose_name_plural = _('bloggers')
-        
+
+class Blog(models.Model):
+    title = models.CharField(_("Blog Title"),max_length=300)
+    description = models.TextField(_("Details : "),null=True,blank=True)
+    created_at = models.DateTimeField(_("Creation Date & Time"),auto_now_add=True)
+    updated_at = models.DateTimeField(_("Last Updated: "),auto_now=True)
+    creator = models.ForeignKey(Blogger,on_delete=models.CASCADE,related_name='blogs')

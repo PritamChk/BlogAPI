ba8bb48dd51e9abd200d75737004fe9601adb459
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-20 17:54:55 +0530

    comment model added

--- blogapp/models.py
+++ blogapp/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class Blogger(AbstractUser):
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_('email address'), unique=True, db_index=True)
     # follows = models.ManyToManyField('self',blank=True,symmetrical=False)
     # followed_by = models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True,related_name='followers')
 
@@ -15,9 +15,43 @@ class Blogger(AbstractUser):
         verbose_name = _('blogger')
         verbose_name_plural = _('bloggers')
 
+
 class Blog(models.Model):
-    title = models.CharField(_("Blog Title"),max_length=300)
-    description = models.TextField(_("Details : "),null=True,blank=True)
-    created_at = models.DateTimeField(_("Creation Date & Time"),auto_now_add=True)
-    updated_at = models.DateTimeField(_("Last Updated: "),auto_now=True)
-    creator = models.ForeignKey(Blogger,on_delete=models.CASCADE,related_name='blogs')
+    title = models.CharField(_("Blog Title"), max_length=300)
+    description = models.TextField(_("Details : "), null=True, blank=True)
+    created_at = models.DateTimeField(
+        _("Creation Date & Time"), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(
+        _("Last Updated: "), auto_now=True, db_index=True)
+    creator = models.ForeignKey(
+        Blogger, on_delete=models.CASCADE, related_name='blogs')
+
+    class Meta:
+        ordering = ["-created_at", "title"]
+
+    def __str__(self) -> str:
+        return f"{self.id} [ {self.title} ]"
+
+    def get_short_description(self) -> str:
+        return self.description[:30]+"..."
+
+
+class Comment(models.Model):
+    boktobyo = models.TextField()
+    created_at = models.DateTimeField(
+        _("Creation Date & Time"), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(
+        _("Last Updated: "), auto_now=True, db_index=True)
+    commentor = models.ForeignKey(
+        Blogger, on_delete=models.CASCADE, related_name='comments')
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, related_name="comments")
+
+    class Meta:
+        ordering = ["-created_at", "updated_at"]
+
+    def __str__(self) -> str:
+        return f"ID: {self.id}"
+
+    def get_short_boktobyo(self) -> str:
+        return f"{self.boktobyo[:15]}..."

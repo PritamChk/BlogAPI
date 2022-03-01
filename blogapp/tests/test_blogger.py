d7c27cd9f627f2c5a27808af99956aaed0699b3c
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-03-01 16:48:04 +0530

    pywatch installed and working

--- blogapp/tests/test_blogger.py
+++ blogapp/tests/test_blogger.py
@@ -1,10 +1,11 @@
-from http import client
 from rest_framework.test import APIClient
-from rest_framework import status 
+from rest_framework import status
+import pytest as ptest
 
+
+@ptest.mark.django_db
 class TestGetBloggerList:
     def test_if_blogger_is_anonymous_returns_401(self):
         client = APIClient()
         response = client.get('/blogapp/bloggers/')
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-        
